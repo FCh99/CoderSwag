d80aaf47b91901967a629d9295739ebb557de381
Fausto Checa <fausto.checa@gmail.com>
2019-01-03 17:42:20 +0100

    to recycler3

--- app/src/main/java/udemy/fausto/com/coderswag/Adapters/ProductsAdapter.kt
+++ app/src/main/java/udemy/fausto/com/coderswag/Adapters/ProductsAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.product_list_item.view.*
 import udemy.fausto.com.coderswag.Model.Product
 
 class ProductsAdapter(val context: Context, val products: List<Product> ) : RecyclerView.Adapter<ProductsAdapter.ProductHolder>() {
@@ -13,7 +14,7 @@ class ProductsAdapter(val context: Context, val products: List<Product> ) : Recy
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return products.count()
     }
 
     override fun onBindViewHolder(holder: ProductHolder, position: Int) {
@@ -22,6 +23,24 @@ class ProductsAdapter(val context: Context, val products: List<Product> ) : Recy
 
 
     inner class ProductHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val productImage = itemView.productImage!!
+        val productName = itemView.productName!!
+        val productPrice = itemView.productPrice!!
+
+        fun bindProduct(product: Product, context: Context) {
+            val resourceId = context.resources.getIdentifier(product.image, "drawable", context.packageName)
+
+            productImage.setImageResource(resourceId)
+            productName.text = product.title
+            productPrice.text = product.price
+
+        }
+
+
+
+
+
 
     }
 
