09f76094e74e199f37b2daf776a6d639aa289b40
Fausto Checa <fausto.checa@gmail.com>
2019-01-03 18:14:36 +0100

    Run and ok

--- app/src/main/java/udemy/fausto/com/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/udemy/fausto/com/coderswag/Controller/ProductsActivity.kt
@@ -2,17 +2,33 @@ package udemy.fausto.com.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_products.*
+import udemy.fausto.com.coderswag.Adapters.ProductsAdapter
 import udemy.fausto.com.coderswag.R
+import udemy.fausto.com.coderswag.Services.DataService
 import udemy.fausto.com.coderswag.Utilities.EXTRA_CATEGORY
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println("-------->")
+        //println("-------->")
         //println(categoryType)
+
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        val layoutManager = GridLayoutManager(this, 2)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
+
+
+
     }
 }
